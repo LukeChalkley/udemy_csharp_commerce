05f841a87ab68f5f7b4e120b9250e8bb2ba5f4e8
Luke Chalkley <lukechalkley@hotmail.com.au>
2023-09-11 20:34:31 +1000

    Added default project files.

---
+++ Bulky/BulkyWebApp/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+/*
+    If we have various named environments, we can use
+        app.Environment.IsEnvironment()
+    which passes an environment name and checks.
+*/
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); // ? refers to nullable. May not be supplied.
+
+app.Run();
